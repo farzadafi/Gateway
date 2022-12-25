718c25919b0676ea9dc40ee1769e40f148c784cc
farzadafi <farzadafi40@gmail.com>
2022-12-25 20:02:34 +0330

    create reverseProxyMethod

--- src/main/java/com/example/gateway/controller/ReverseController.java
+++ src/main/java/com/example/gateway/controller/ReverseController.java
@@ -1,4 +1,29 @@
 package com.example.gateway.controller;
 
+import com.example.gateway.service.ProxyService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.net.URISyntaxException;
+import java.util.UUID;
+
 public class ReverseController {
+
+    private final ProxyService proxyService;
+
+    public ReverseController(ProxyService proxyService) {
+        this.proxyService = proxyService;
+    }
+
+    @RequestMapping("/**")
+    public ResponseEntity<?> reverseProxyMethod(@RequestBody(required = false) String body,
+                                              HttpMethod method,
+                                              HttpServletRequest request,
+                                              HttpServletResponse response) throws URISyntaxException {
+        return proxyService.processProxyRequest(body, method, request, response, UUID.randomUUID().toString());
+    }
 }
