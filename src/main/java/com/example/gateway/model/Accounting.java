0def67db07ce8761a88f605e9bb33c283e7d7ef0
farzadafi <farzadafi40@gmail.com>
2022-12-26 18:37:30 +0330

    add lombok annotation and redisHash

--- src/main/java/com/example/gateway/model/Accounting.java
+++ src/main/java/com/example/gateway/model/Accounting.java
@@ -1,4 +1,14 @@
 package com.example.gateway.model;
 
+import lombok.*;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@RedisHash("Accounting")
 public class Accounting {
 }
