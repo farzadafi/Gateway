3d28a31ad5bb23d30b8ce7ab469ed4da3ae5942b
farzadafi <farzadafi40@gmail.com>
2022-12-26 18:54:08 +0330

    add some field need for accounting

--- src/main/java/com/example/gateway/model/Accounting.java
+++ src/main/java/com/example/gateway/model/Accounting.java
@@ -1,7 +1,10 @@
 package com.example.gateway.model;
 
+import jakarta.persistence.Column;
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+
 
 @Getter
 @Setter
@@ -11,4 +14,23 @@ import org.springframework.data.redis.core.RedisHash;
 @ToString
 @RedisHash("Accounting")
 public class Accounting {
+    @Id
+    @Column(nullable = false)
+    private String id;
+
+    private String user;
+
+    private String urlCalled;
+
+    private String userAgent;
+
+    private String ip;
+
+    private String referer;
+
+    private String method;
+
+    private String bodyParameter;
+
+    private String requestParameter;
 }
