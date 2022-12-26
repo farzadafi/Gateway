16d9ed54974435d98f069a07cdef9a6a161d78ef
farzadafi <farzadafi40@gmail.com>
2022-12-26 19:06:20 +0330

    fix reverseProxyMethod and add accounting to mehtod

--- src/main/java/com/example/gateway/controller/ReverseController.java
+++ src/main/java/com/example/gateway/controller/ReverseController.java
@@ -1,6 +1,7 @@
 package com.example.gateway.controller;
 
 import com.example.gateway.service.ProxyService;
+import com.example.gateway.service.accounting.AccountingServiceImpel;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpMethod;
@@ -14,9 +15,11 @@ import java.util.UUID;
 public class ReverseController {
 
     private final ProxyService proxyService;
+    private final AccountingServiceImpel accountingServiceImpel;
 
-    public ReverseController(ProxyService proxyService) {
+    public ReverseController(ProxyService proxyService, AccountingServiceImpel accountingServiceImpel) {
         this.proxyService = proxyService;
+        this.accountingServiceImpel = accountingServiceImpel;
     }
 
     @RequestMapping("/**")
@@ -24,6 +27,7 @@ public class ReverseController {
                                               HttpMethod method,
                                               HttpServletRequest request,
                                               HttpServletResponse response) throws URISyntaxException {
+        accountingServiceImpel.save(request,method.toString(),body);
         return proxyService.processProxyRequest(body, method, request, response, UUID.randomUUID().toString());
     }
 }
