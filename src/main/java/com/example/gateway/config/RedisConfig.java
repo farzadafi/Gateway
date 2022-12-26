374c863ceb85a5113860d1b35f094584f9c22c73
farzadafi <farzadafi40@gmail.com>
2022-12-26 18:33:40 +0330

    add jedisConnectionFactory method

--- src/main/java/com/example/gateway/config/RedisConfig.java
+++ src/main/java/com/example/gateway/config/RedisConfig.java
@@ -1,4 +1,12 @@
 package com.example.gateway.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
 public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        return new JedisConnectionFactory();
+    }
 }
