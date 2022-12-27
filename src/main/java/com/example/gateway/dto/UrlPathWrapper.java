e2abca9157920de86b7f49547cb1a21789d5cc90
farzadafi <farzadafi40@gmail.com>
2022-12-27 18:24:55 +0330

    add some lombok annotation and add one list

--- src/main/java/com/example/gateway/dto/UrlPathWrapper.java
+++ src/main/java/com/example/gateway/dto/UrlPathWrapper.java
@@ -1,4 +1,14 @@
 package com.example.gateway.dto;
 
+import com.example.gateway.model.UrlPath;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
 public class UrlPathWrapper {
+
+    private List<UrlPath> urlPaths;
 }
