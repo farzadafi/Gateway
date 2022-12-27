0c3e5103f06694c8a3af4c3342f51000c71b8885
farzadafi <farzadafi40@gmail.com>
2022-12-27 18:20:27 +0330

    add dtoToModel method

--- src/main/java/com/example/gateway/mapper/UrlPathMapper.java
+++ src/main/java/com/example/gateway/mapper/UrlPathMapper.java
@@ -1,4 +1,14 @@
 package com.example.gateway.mapper;
 
+import com.example.gateway.dto.UrlPathDto;
+import com.example.gateway.model.UrlPath;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UrlPathMapper {
+
+    UrlPathMapper INSTANCE = Mappers.getMapper(UrlPathMapper.class);
+
+    UrlPath dtoToModel(UrlPathDto urlPathDto);
 }
