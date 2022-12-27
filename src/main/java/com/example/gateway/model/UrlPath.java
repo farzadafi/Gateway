615a369a78021582026e380adcb869bd84d7c99c
farzadafi <farzadafi40@gmail.com>
2022-12-27 17:46:07 +0330

    add some field

--- src/main/java/com/example/gateway/model/UrlPath.java
+++ src/main/java/com/example/gateway/model/UrlPath.java
@@ -1,4 +1,14 @@
 package com.example.gateway.model;
 
+
+import jakarta.persistence.Column;
+import org.springframework.data.annotation.Id;
+
 public class UrlPath {
+    @Id
+    @Column(nullable = false)
+    private String source;
+
+    @Column(nullable = false)
+    private String destination;
 }
