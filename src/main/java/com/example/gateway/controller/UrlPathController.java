3b78594e0adfcf6d41275a7396782a6a23cd38c0
farzadafi <farzadafi40@gmail.com>
2022-12-27 18:22:41 +0330

    add addUrlPath method

--- src/main/java/com/example/gateway/controller/UrlPathController.java
+++ src/main/java/com/example/gateway/controller/UrlPathController.java
@@ -1,4 +1,26 @@
 package com.example.gateway.controller;
 
+import com.example.gateway.dto.UrlPathDto;
+import com.example.gateway.mapper.UrlPathMapper;
+import com.example.gateway.model.UrlPath;
+import com.example.gateway.service.urlPath.UrlPathServiceImpel;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UrlPathController {
+
+    private final UrlPathServiceImpel urlPathServiceImpel;
+
+    public UrlPathController(UrlPathServiceImpel urlPathServiceImpel) {
+        this.urlPathServiceImpel = urlPathServiceImpel;
+    }
+
+    @PostMapping("/urlPath/add")
+    public String addUrlPath(@RequestBody UrlPathDto urlPathDto) {
+        UrlPath urlPath = UrlPathMapper.INSTANCE.dtoToModel(urlPathDto);
+        urlPathServiceImpel.save(urlPath);
+        return "OK";
+    }
 }
