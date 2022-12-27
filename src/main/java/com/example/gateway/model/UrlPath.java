dd960945572716ce4399b38e68a22add1358467e
farzadafi <farzadafi40@gmail.com>
2022-12-27 17:46:55 +0330

    add lombok annotation and redisHash and implements from Serializable

--- src/main/java/com/example/gateway/model/UrlPath.java
+++ src/main/java/com/example/gateway/model/UrlPath.java
@@ -2,9 +2,20 @@ package com.example.gateway.model;
 
 
 import jakarta.persistence.Column;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
 
-public class UrlPath {
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@RedisHash("UrlPath")
+public class UrlPath implements Serializable {
     @Id
     @Column(nullable = false)
     private String source;
