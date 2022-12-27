0b0f0f361394f345d435b470a8dae17262d8d130
farzadafi <farzadafi40@gmail.com>
2022-12-27 18:29:31 +0330

    add findByUrl method

--- src/main/java/com/example/gateway/controller/UrlPathController.java
+++ src/main/java/com/example/gateway/controller/UrlPathController.java
@@ -2,12 +2,11 @@ package com.example.gateway.controller;
 
 import com.example.gateway.dto.UrlPathDto;
 import com.example.gateway.dto.UrlPathWrapper;
+import com.example.gateway.exception.UrlNotFoundException;
 import com.example.gateway.mapper.UrlPathMapper;
 import com.example.gateway.model.UrlPath;
 import com.example.gateway.service.urlPath.UrlPathServiceImpel;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UrlPathController {
@@ -29,5 +28,11 @@ public class UrlPathController {
     public String saveAll(@RequestBody UrlPathWrapper urlPathWrapper) {
         urlPathServiceImpel.saveAll(urlPathWrapper);
         return "OK";
+    }
+
+    @GetMapping("/urlPath/findByUrl")
+    public UrlPath findByUrl(@RequestParam String url) {
+        return urlPathServiceImpel.findBySource(url)
+                .orElseThrow(() -> new UrlNotFoundException("this url not found!"));
     }
 }
