6b3f71dcead77448acd5f63eedb76cc516626bc5
farzadafi <farzadafi40@gmail.com>
2022-12-28 09:49:04 +0330

    extend from RuntimeException

--- src/main/java/com/example/gateway/exception/MethodNotAllowedException.java
+++ src/main/java/com/example/gateway/exception/MethodNotAllowedException.java
@@ -1,4 +1,12 @@
 package com.example.gateway.exception;
 
-public class MethodNotAllowedException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "send true method")
+public class MethodNotAllowedException extends RuntimeException{
+
+    public MethodNotAllowedException(String message) {
+        super(message);
+    }
 }
