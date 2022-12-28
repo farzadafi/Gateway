1ea4387104986253c890376a79524baf6f772b03
farzadafi <farzadafi40@gmail.com>
2022-12-28 10:16:02 +0330

    add dozerBeanMapper Bean

--- src/main/java/com/example/gateway/config/Config.java
+++ src/main/java/com/example/gateway/config/Config.java
@@ -1,5 +1,6 @@
 package com.example.gateway.config;
 
+import org.dozer.DozerBeanMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +11,10 @@ public class Config {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public DozerBeanMapper dozerBeanMapper(){
+        return new DozerBeanMapper();
     }
 }
