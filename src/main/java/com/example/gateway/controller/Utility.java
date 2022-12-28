01ac7957e1727758564ea07d2ab6f202af14086e
farzadafi <farzadafi40@gmail.com>
2022-12-28 09:45:49 +0330

    add processGetParameter method

--- src/main/java/com/example/gateway/controller/Utility.java
+++ src/main/java/com/example/gateway/controller/Utility.java
@@ -1,9 +1,32 @@
 package com.example.gateway.controller;
 
+import com.example.gateway.exception.InvalidRequestException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 @Component
 public class Utility {
 
+    public ArrayList<String> processGetParameter(String parameter, ArrayList<String> parameterName) {
+        parameterName = addEqualSignToEndEachElement(parameterName);
+        ArrayList<String> parameterValue = new ArrayList<>();
+        try {
+            String[] split = parameter.split("&");
+            for (int i = 0; i < parameterName.size(); i++) {
+                String[] Array = split[i].split(parameterName.get(i));
+                parameterValue.add(i, Array[1]);
+            }
+        } catch (Exception e) {
+            throw new InvalidRequestException("send valid parameter");
+        }
+        return parameterValue;
+    }
+
+    public ArrayList<String> addEqualSignToEndEachElement(ArrayList<String> parameterName) {
+        return (ArrayList<String>) parameterName.stream()
+                .map(f -> f + "=")
+                .collect(Collectors.toList());
+    }
 }
