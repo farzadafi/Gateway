7004024eff2da79d94ff790c37b050a01ddcb0f4
farzadafi <farzadafi40@gmail.com>
2022-12-28 09:56:28 +0330

    add test method

--- src/main/java/com/example/gateway/controller/TestController.java
+++ src/main/java/com/example/gateway/controller/TestController.java
@@ -1,6 +1,30 @@
 package com.example.gateway.controller;
 
+import com.example.gateway.exception.InvalidRequestException;
+import com.google.common.collect.Lists;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+
 public class TestController {
 
+    private final Utility utility;
 
+    public TestController(Utility utility) {
+        this.utility = utility;
+    }
+
+    public ResponseEntity<String> test(String queryParameter, String body, String method) {
+        if (method.equals("GET"))
+            return (ResponseEntity<?>) ResponseEntity.badRequest();
+        if (queryParameter == null)
+            throw new InvalidRequestException("send valid parameter");
+
+        ArrayList<String> parameterName = Lists.newArrayList("firstname", "lastname");
+        ArrayList<String> parameterValue = utility.processGetParameter(queryParameter, parameterName);
+        String firstname = parameterValue.get(0);
+        String lastname  = parameterValue.get(1);
+
+        return ResponseEntity.ok(String.format("%s %s", firstname, lastname));
+    }
 }
