5d5025014af40c8ed2ca8c9d5f8e17f3f440dfc3
farzadafi <farzadafi40@gmail.com>
2022-12-31 21:25:05 +0330

    fix processProxyRequest method and add some reflection

--- src/main/java/com/example/gateway/service/ProxyService.java
+++ src/main/java/com/example/gateway/service/ProxyService.java
@@ -1,8 +1,11 @@
 package com.example.gateway.service;
 
+import com.example.gateway.model.UrlPath;
+import com.example.gateway.service.urlPath.UrlPathServiceImpel;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.ThreadContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -14,19 +17,45 @@ import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
 
 public class ProxyService {
+
+    private final UrlPathServiceImpel urlPathServiceImpel;
+    private final ApplicationContext context;
+
+    public ProxyService(UrlPathServiceImpel urlPathServiceImpel, ApplicationContext context) {
+        this.urlPathServiceImpel = urlPathServiceImpel;
+        this.context = context;
+    }
 
     public ResponseEntity<?> processProxyRequest(String body,
                                                  HttpMethod method,
                                                  HttpServletRequest request,
                                                  HttpServletResponse response,
-                                                 String traceId) throws URISyntaxException {
+                                                 String traceId) throws URISyntaxException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         ThreadContext.put("traceId", traceId);
         URI uri = createUri(request);
+        String requestUrl = request.getRequestURI();
+
+        Optional<UrlPath> urlPath = urlPathServiceImpel.findBySource(requestUrl);
+        if (urlPath.isPresent()) {
+            requestUrl = urlPath.get().getDestination();
+            List<String> address = List.of(requestUrl.split("\\."));
+            String className = address.get(0);
+            String methodName = address.get(1);
+
+            Object myClassObject = context.getBean(className);
+            Method methodCalled;
+            methodCalled = myClassObject.getClass().getMethod(methodName, String.class, String.class, String.class);
+            return (ResponseEntity<?>) methodCalled.invoke(myClassObject, request.getQueryString(), body, method.name());
+        }
 
         HttpHeaders headers = createHttpHeaders(request, traceId);
 
